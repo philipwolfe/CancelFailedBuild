c787e0b0e8a40ad52c4edd33c24db8391be29ad4
Philip Wolfe <philip.wolfe@orion.com>
2022-08-16 17:10:02 -0500

    Updating for VS 2022

--- CancelFailedBuild.cs
+++ CancelFailedBuild.cs
@@ -1,9 +1,10 @@
-using System.ComponentModel.Composition;
-using System.Text.RegularExpressions;
+using System;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.Text.RegularExpressions;
 
 namespace CancelFailedBuild
 {
@@ -15,23 +16,29 @@ namespace CancelFailedBuild
         [Import]
         SVsServiceProvider GlobalServiceProvider = null;
 
-        static readonly Regex BuildError = new Regex(": error|: fatal error", RegexOptions.IgnoreCase);
+        private static readonly Regex BuildError = new Regex(": error|: fatal error", RegexOptions.IgnoreCase);
 
         public void TextViewCreated(IWpfTextView textView)
         {
-            var dte = GlobalServiceProvider.GetService(typeof(DTE)) as DTE;
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = (DTE)GlobalServiceProvider.GetService(typeof(DTE));
+
             textView.TextBuffer.Changed += (sender, args) =>
+            {
+                //Output window is friendly and writes full lines at a time, so we only need to look at the changed text.
+                foreach (var change in args.Changes)
                 {
-                    //Output window is friendly and writes full lines at a time, so we only need to look at the changed text.
-                    foreach (var change in args.Changes)
+                    string text = args.After.GetText(change.NewSpan);
+                    if (BuildError.IsMatch(text))
                     {
-                        string text = args.After.GetText(change.NewSpan);
-                        if (BuildError.IsMatch(text))
-                        {
-                            dte.ExecuteCommand("Build.Cancel");
-                        }
+                        ThreadHelper.ThrowIfNotOnUIThread();
+                        dte.ExecuteCommand("Build.Cancel");
+
+                        textView.TextBuffer.Insert(textView.TextBuffer.CurrentSnapshot.Length, "Build cancelled by the cancel on first failure extension." + Environment.NewLine);
                     }
-                };
+                }
+            };
+
         }
     }
 }
